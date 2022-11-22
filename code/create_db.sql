1c8ed7cd45ca057bf925046bdf1692f5e165cf26
diegovaldiviac <valdiviacox.d@northeastern.edu>
2022-11-21 21:42:31 -0500

    user tables added

--- code/create_db.sql
+++ code/create_db.sql
@@ -14,66 +14,80 @@ CREATE TABLE PERSON (
     firstName TEXT NOT NULL,
     middleName TEXT,
     lastName TEXT NOT NULL,
+    email TEXT UNIQUE NOT NULL,
     state CHAR(4),
-    zipCode INTEGER
-    driversID INTEGER
+    zipCode INTEGER,
+    restriction CHAR(4),
+    FOREIGN KEY driversID INTEGER
 );
+
+CREATE TABLE MISC (
+    organDonor BOOLEAN,
+    veteran BOOLEAN,
+    FOREIGN KEY driversID INTEGER
+)
+
 
 CREATE TABLE BIO_INFORMATION (
     -- One of M, F, X (None Binary)
     Gender CHAR(1),
-    Eye_Color CHAR(4),
-    Hair_Color Char(4),
-    Blood_Type CHAR(4),
-    Weight FLOAT,
-    Height FLOAT,
+    eyeColor CHAR(4),
+    hairColor Char(4),
+    bloodType CHAR(4),
+    weight FLOAT,
+    height FLOAT,
 );
 
 CREATE TABLE PICTURE (
     -- TODO: How to represent a picture correctly
-    -- TODO: Foreign Key Constraints
-    Picture_Address CHAR(4)
+    pictureAddress CHAR(4),
+    FOREIGN KEY driversID INTEGER
+
 );
 
 CREATE TABLE USER_CREDENTIALS (
-    Nickname CHAR(4),
-    Password int NOT NULL,
-    -- TODO: Foreign Key Constraints
+    nickname CHAR(4),
+    password int NOT NULL,
+    FOREIGN KEY driversID INTEGER
 );
 
 
 CREATE TABLE INCIDENT_HISTORY (
     -- One of A, B, C, D, E, F
-    Incident_Type CHAR(1) NOT NULL,
-    Incident_Date DATE NOT NULL,
-    Fine_Amount FLOAT NOT NULL
+    incidentType CHAR(1) NOT NULL,
+    incidentDate DATE NOT NULL,
+    fineAmount FLOAT NOT NULL,
+    FOREIGN KEY driversID INTEGER
 );
 
 
 CREATE TABLE EMERGENCY_CONTACT_INFO (
     firstName TEXT,
     lastName TEXT,
-    phoneNumber INTEGER
+    phoneNumber INTEGER,
+    FOREIGN KEY driversID INTEGER
 );
 
 
 CREATE TABLE PRE_VALIDATION_DATA (
     roadTestResult TEXT,
     statusOfLearnersPermit TEXT,
-    dmvOffice TEXT
+    dmvOffice TEXT,
+    FOREIGN KEY driversID INTEGER
 );
 
 CREATE TABLE LICENSE_ASSOCIATED_INFORMATION (
     issueDate DATE,
     expirationDate DATE,
     VehicleType CHAR(1),
-    driversID INTEGER
+    FOREIGN KEY driversID INTEGER
 
 );
 CREATE TABLE VEHICLE_INFORMATION (
     vehicleType TEXT,
     description TEXT,
-    licensePlate TEXT
+    licensePlate TEXT,
+    FOREIGN KEY driversID INTEGER
 );
 
 
